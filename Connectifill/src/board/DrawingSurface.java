026d2be0f2c9644f01354452088406758a7745a5
rushil-jay <rushil.jayant@gmail.com>
2021-05-07 14:52:21 -0700

    Empty GUI

---
+++ Connectifill/src/board/DrawingSurface.java
@@ -0,0 +1,38 @@
+package board;
+
+
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.geom.Point2D;
+
+import javax.swing.JFrame;
+
+import processing.core.PApplet;
+
+
+
+public class DrawingSurface extends PApplet {
+	
+	public DrawingSurface() {
+	}
+	
+
+	public void draw() { 
+		background(255);   
+		fill(0);
+		textAlign(LEFT);
+		textSize(12);
+	
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
