83ab39eda75d36ef00eb6bb5b768aa4567cc3e6d
Srikar Gudipati <43484884+Boss17000@users.noreply.github.com>
2021-05-13 18:21:57 -0700

    Added Main.java

--- Connectifill/src/graphics/Board.java
+++ Connectifill/src/graphics/Board.java
@@ -1,26 +1,43 @@
 package graphics;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.geom.Point2D;
 
-public class Board 
-{
-	private static int row=12;
-	private static int column = 6;
-	Piece [][]grid = new Piece[row][column];
-	
-	public Display()
+import javax.swing.JFrame;
+
+import processing.core.PApplet;
+
+
+
+public class Board extends PApplet {
+
+	int width = 12; 
+	int height = 6;
+	int size = 100;	
+	int[][]board = new int[height][width];
+	public void setup()
 	{
-		for(int r = 0; r < row;r++)
-		{
-			
-		}
+		size(1200,600);
+		ellipseMode(CORNER);
 	}
-	public Board()
-	{
-		for(int r = 0; r<row; r++)
+	public void draw() { 
+		//background(255);
+		for(int i = 0; i<height;i++)
 		{
-			for(int c = 0; c<column; c++)
+			for(int j = 0; j<width;j++)
 			{
-				grid[r][c] = null;
+				fill(255);
+				rect(j*size,i*size,size,size);
+				if(board[i][j]>0)
+				{
+					fill(board[i][j]==1?255:0 , board[i][j]==2?255:0,0);
+					ellipse(j*size,i*size,size,size);
+				}
 			}
 		}
+		//textAlign(LEFT);
+		//textSize(12);
+	
+		
 	}
 }
