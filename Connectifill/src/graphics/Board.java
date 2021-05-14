04d5f1236032b7391f817e47eba4c2aed24490e1
Srikar Gudipati <43484884+Boss17000@users.noreply.github.com>
2021-05-13 18:13:30 -0700

    Updated Board.java and Piece.java

--- Connectifill/src/graphics/Board.java
+++ Connectifill/src/graphics/Board.java
@@ -2,10 +2,25 @@ package graphics;
 
 public class Board 
 {
-
-	public Board() 
+	private static int row=12;
+	private static int column = 6;
+	Piece [][]grid = new Piece[row][column];
+	
+	public Display()
 	{
-		// TODO Auto-generated constructor stub
+		for(int r = 0; r < row;r++)
+		{
+			
+		}
 	}
-
+	public Board()
+	{
+		for(int r = 0; r<row; r++)
+		{
+			for(int c = 0; c<column; c++)
+			{
+				grid[r][c] = null;
+			}
+		}
+	}
 }
--- Connectifill/src/graphics/Piece.java
+++ Connectifill/src/graphics/Piece.java
@@ -1,9 +1,15 @@
 package graphics;
 
-public class Piece {
-
-	public Piece() {
-		// TODO Auto-generated constructor stub
+public class Piece 
+{
+	private String color;
+	
+	public String getColor()
+	{
+		return color;
 	}
-
+	public void setColor(String color)
+	{
+		this.color = color;
+	}
 }
