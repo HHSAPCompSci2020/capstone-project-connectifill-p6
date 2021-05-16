a5af86a7b351ebd69cbca6038dedd6fbc0d27381
rushil-jay <rushil.jayant@gmail.com>
2021-05-15 17:54:28 -0700

    Board Class Updated

--- Connectifill/src/graphics/Game.java
+++ Connectifill/src/graphics/Game.java
@@ -11,33 +11,15 @@ public class Game extends PApplet
 	private Menu menu;
 	private Board board;
 
-	private int h;
-	private int w;
 	private int standard;
-
-	private int[][] grid;
 
 	private boolean gameState;
 
 	public Game()
 	{
-		menu = new Menu();
-		board = new Board();
+		board = new Board(12, 6);
 		gameState = true;
-		h = 6;
-		w = 12;
 		standard = 100;
-		grid = new int[6][12];
-	}
-
-	public Menu getMenu()
-	{
-		return menu;
-	}
-
-	public Board getBoard()
-	{
-		return board;
 	}
 
 	public void setup()
@@ -49,18 +31,18 @@ public class Game extends PApplet
 	{
 		if (gameState)
 		{
-			for (int j = 0; j < h; j++)
+			for (int j = 0; j < board.getHeight(); j++)
 			{
-				for (int i = 0; i < w; i++)
+				for (int i = 0; i < board.getWidth(); i++)
 				{
 					fill(255);
 					rect(i*standard, j*standard, standard, standard);
-					if (grid[j][i] == 1)
+					if (board.getValue(j, i) == 1)
 					{
 						fill(0,255,0);
 						ellipse(i*standard, j*standard, standard, standard);
 					}
-					else if (grid[j][i] == 2)
+					else if (board.getValue(j, i) == 2)
 					{
 						fill(255,0,0);
 						ellipse(i*standard, j*standard, standard, standard);
@@ -74,104 +56,29 @@ public class Game extends PApplet
 			fill(255);
 			textAlign(CENTER);
 		}
-
-
 	}
 
 	public void mousePressed() {
 		int column = mouseX/standard;
 
-		if(add(column, 1))
+		if(board.add(column, 1))
 		{
 			pickRandom();
 		}
-		System.out.println("Player: " + getScore(1) + " | Computer: " + getScore(2));
-	}
-
-	public boolean add(int column, int player)
-	{
-		if (addable(column))
-		{
-			grid[nextOpenRow(column)][column] = player;
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	public boolean addable(int column)
-	{
-		int filled = 0;
-		for (int i = grid.length - 1; i >= 0; i--)
-		{
-			if (grid[i][column] != 0)
-			{
-				filled++;
-			}
-		}
-		if (filled == grid.length)
-		{
-			return false;
-		}
-		return true;
-	} 
-
-	// Do diagonal scoring
-	public int getScore(int player)
-	{
-		int score = 0;
-		for (int i = 0; i <= grid[0].length-4; i++)
-		{
-			for (int j = 0; j < grid.length; j++)
-			{
-				if (grid[j][i] == player && grid[j][i + 1] == player && grid[j][i + 2] == player && grid[j][i + 3] == player)
-				{
-					score++;
-				}
-			}
-		}
-		for (int x = 0; x < grid[0].length; x++)
-		{
-			for (int y = 0; y < grid.length-3; y++)
-			{
-				if (grid[y][x] == player && grid[y + 1][x] == player && grid[y + 2][x] == player && grid[y + 3][x] == player)
-				{
-					score++;
-				}
-			}
-		}
-		return score;
-	}
-
-	public int nextOpenRow(int column)
-	{
-		int filled = 1;
-		for (int i = grid.length - 1; i >= 0; i--)
-		{
-			if (grid[i][column] == 0)
-			{
-				return (grid.length-filled);
-			}
-			if (grid[i][column] != 0)
-			{
-				filled++;
-			}
-		}
-		return grid.length-1;
+		System.out.println("Player: " + board.getScore(1) + " | Computer: " + board.getScore(2));
 	}
 
 	public void pickRandom() {
 		int column = (int) (Math.random() * 12);
-		if (addable(column))
+		if (board.addable(column))
 		{
-			add(column, 2);
+			board.add(column, 2);
 		}
 		else {
 			pickRandom();
 		}
 	}
+
 	public static void main(String args[])
 	{
 
@@ -181,7 +88,7 @@ public class Game extends PApplet
 		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
 		JFrame window = (JFrame)canvas.getFrame();
 
-		window.setSize(1200, 650);
+		window.setSize(1200, 650); // Fix Dimensions
 		window.setMinimumSize(new Dimension(100,100));
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(true);
