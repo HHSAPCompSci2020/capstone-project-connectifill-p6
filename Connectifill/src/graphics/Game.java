88dee6f7955a18373bb805c5f629bcf8fc0390dc
rushil-jay <rushil.jayant@gmail.com>
2021-05-24 23:14:01 -0700

    Menu Fixed

--- Connectifill/src/graphics/Game.java
+++ Connectifill/src/graphics/Game.java
@@ -2,8 +2,10 @@ package graphics;
 
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import models.AggressiveModel;
+import models.CombinedModel;
 import models.DefensiveModel;
 import models.MiniMaxModel;
 import models.Model;
@@ -25,7 +27,6 @@ public class Game extends PApplet
 	public Game()
 	{
 		board = new Board(24, 12);
-		model = new DefensiveModel();
 		gameState = true;
 		standard = 50;
 	}
@@ -42,8 +43,9 @@ public class Game extends PApplet
 		textFont(scoreboard,32); 
 		background(255);
 		fill(0);
+
 		text("Player: " + board.getScore(1) +" | Computer: " + board.getScore(2),452,650);
-		
+
 		if (gameState)
 		{
 			for (int j = 0; j < board.getHeight(); j++)
@@ -70,9 +72,9 @@ public class Game extends PApplet
 			background(255);   // Clear the screen with a white background
 			fill(255);
 			textAlign(CENTER);
-			
+
 		}
-		
+
 	}
 
 	public void mousePressed() 
@@ -84,12 +86,12 @@ public class Game extends PApplet
 			model.makeMove();
 		}
 	}
-	
+
 	public static Board getBoard()
 	{
 		return board;
 	}
-	
+
 	public static Model getModel()
 	{
 		return model;
@@ -97,6 +99,29 @@ public class Game extends PApplet
 
 	public static void main(String args[])
 	{
+		String[] options = {"Random Model", "MiniMax Model", "Defensive Model", "Combined Model", "Aggressive Model"};
+		String n = (String)JOptionPane.showInputDialog(null, "What model do you want to play against?", 
+				"Connectifill", JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+		if (n.equals(options[0]))
+		{
+			model = new RandomModel();
+		}
+		if (n.equals(options[1]))
+		{
+			model = new MiniMaxModel();
+		}
+		if (n.equals(options[2]))
+		{
+			model = new DefensiveModel();
+		}
+		if (n.equals(options[3]))
+		{
+			model = new CombinedModel();
+		}
+		if (n.equals(options[4]))
+		{
+			model = new AggressiveModel();
+		}
 
 		Game drawing = new Game();
 		PApplet.runSketch(new String[]{""}, drawing);
